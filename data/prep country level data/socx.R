09224e208949a8728e6236b17cc727a40d647ff6
Nate Breznau <breznau.nate@gmail.com>
2022-03-07 17:51:24 +0100

    * Improved the SOCX data flow * Updated Figs 1&2 * New Table 1 and new by country-wave sample size table

--- data/prep country level data/socx.R
+++ data/prep country level data/socx.R
@@ -2,19 +2,15 @@ pacman::p_load("tidyverse")
 
 
 # OECD https://stats.oecd.org/Index.aspx?datasetcode=SOCX_AGG#
-socx <- read_csv(here::here("data/prep country level data","SOCX_AGG_26072021091623933.csv"))
+socx <- read_csv(here::here("data/prep country level data","SOCX_AGG_07032022163148772.csv"))
 
-socx1 <- socx %>%
-  subset(Measure == "Per head, at current prices and current PPPs, in US dollars" & Source == "Public") %>%
+
+socx <- socx %>%
+  subset(Measure == "In percentage of Gross Domestic Product" & 
+           Source == "Public" & 
+           `Type of Expenditure` == "Total") %>%
   dplyr::select(COUNTRY, Year, Value)
 
-socx2 <- socx %>%
-  subset(Measure == "In percentage of Gross Domestic Product" & Source == "Public") %>%
-  dplyr::select(COUNTRY, Year, Value)
-
-colnames(socx1) <- c("iso3c","year","socx_perhead")
-colnames(socx2) <- c("iso3c","year","socx")
-
-socx <- full_join(socx1, socx2, by = c("iso3c","year"))
+colnames(socx) <- c("iso3c","year","socx")
 
 write.csv(socx, here::here("data","socx_oecd.csv"), row.names = F)
